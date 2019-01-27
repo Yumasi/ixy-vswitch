657d16c1b398891b2ac0231171563c27c95bd8a1
Guillaume Pagnoux <guillaume.pagnoux@lse.epita.fr>
2019-01-27 12:40:26 +0100

    add frame flooding
    Signed-off-by: Guillaume Pagnoux <guillaume.pagnoux@lse.epita.fr>

--- src/main.cc
+++ src/main.cc
@@ -1,9 +1,38 @@
 #include <iostream>
 #include <vector>
 
+#include "conditional_iterator.hxx"
 #include "interface.hh"
 
 #define BATCH_SIZE (32)
+
+static void flooding(std::vector<Interface>& interfaces)
+{
+    for (Interface& i : interfaces) {
+        pkt_buf* bufs[BATCH_SIZE];
+        uint32_t num_rx = ixy_rx_batch(i.device, 0, bufs, BATCH_SIZE);
+
+        if (num_rx > 0) {
+            std::cout << "Received: " << num_rx << " packets" << std::endl;
+            for (uint32_t i = 0; i < num_rx; ++i)
+                bufs[i]->ref_count = interfaces.size() - 1;
+
+            auto condition = [&](Interface& other) { return other != i; };
+            ConditionalIterator it(interfaces.begin(), interfaces.end(),
+                                   std::ref(condition));
+
+            for (; it.itr != it.end; ++it) {
+                uint32_t num_tx = ixy_tx_batch(it->device, 0, bufs, num_rx);
+                std::cout << "Sent: " << num_tx << " packets" << std::endl;
+                for (uint32_t i = num_tx; i < num_rx; ++i) {
+                    bufs[i]->ref_count = 1;
+                    pkt_buf_free(bufs[i]);
+                }
+            }
+
+        }
+    }
+}
 
 static void forward(Interface& int_1, Interface& int_2)
 {
@@ -33,7 +62,7 @@ int main(int argc, char *argv[])
     }
 
     while (true)
-        forward(interfaces[0], interfaces[1]);
+        flooding(interfaces);
 
     return 0;
 }
